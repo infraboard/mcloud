ecb3d7837620c2e162cba0307324dba84d64af18
yumaojun <18108053819@163.com>
2023-10-23 21:01:51 +0800

    调整样式

--- src/views/console/terminal/TerminalPage.vue
+++ src/views/console/terminal/TerminalPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
+import { app } from '@/stores/localstorage'
 
 const router = useRouter()
 const query = router.currentRoute.value.query
@@ -11,7 +12,7 @@ const logOption = reactive({
   cluster_id: query.cluster_id,
   namespace: query.namespace,
   pod_name: query.pod_name,
-  container_name: ''
+  container_name: '',
 })
 
 // 更新数据
@@ -31,44 +32,58 @@ watch(
 </script>
 
 <template>
-  <div>
-    <a-space class="headline">
-      <!-- <a-space>
-        <a-button type="outline" size="small">
+  <div class="content">
+    <div class="headline">
+      <a-space>
+        <a-radio-group size="mini" type="button" v-model="consoleType" default-value="log">
+          <a-radio value="log"><icon-file /> 日志</a-radio>
+          <a-radio value="console"><icon-desktop /> 控制台</a-radio>
+        </a-radio-group>
+        <a-button type="text" size="mini">
           <template #icon>
             <icon-tool />
           </template>
           工具箱
         </a-button>
-      </a-space> -->
-      <a-space>
-        <a-radio-group size="small" type="button" v-model="consoleType" default-value="log">
-          <a-radio value="log"><icon-file /> 日志</a-radio>
-          <a-radio value="console"><icon-desktop /> 控制台</a-radio>
-        </a-radio-group>
       </a-space>
-    </a-space>
+      <a-space>
+        <a-radio-group size="mini" type="button" v-model="app.xterm.theme" default-value="GitHub">
+            <a-radio value="Solarized_Darcula"><icon-moon-fill /></a-radio>
+            <a-radio value="GitHub"><icon-sun-fill /></a-radio>
+          </a-radio-group>
+      </a-space>
+    </div>
 
-    <PodLog
+
+      <PodLog
       v-if="consoleType === 'log'"
       :key="ts"
       :option="logOption"
-      :width="'calc(100vw - 260px)'"
+      :width="'calc(100vw - 272px)'"
       :height="'calc(100vh - 80px)'"
+      :theme="app.xterm.theme"
     ></PodLog>
     <PodConsole
       v-if="consoleType === 'console'"
       :key="ts"
       :option="logOption"
-      :width="'calc(100vw - 260px)'"
+      :width="'calc(100vw - 272px)'"
       :height="'calc(100vh - 80px)'"
+      :theme="app.xterm.theme"
     ></PodConsole>
+
   </div>
 </template>
 
 <style scoped>
 .headline {
   height: 40px;
-  padding: 0 12px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.content {
+  padding: 0px 6px;
 }
 </style>
