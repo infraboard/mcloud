688d1d77d9ae5b3ea60fdef5629da16ac5a2ef40
yumaojun03 <719118794@qq.com>
2024-05-21 19:24:18 +0800

    content补充滚动条

--- src/views/develop/service/CreateBuildConf.vue
+++ src/views/develop/service/CreateBuildConf.vue
@@ -3,86 +3,88 @@
     <!-- 页头 -->
     <a-page-header :title="pageHeader" @back="router.go(-1)"> </a-page-header>
 
-    <a-card>
-      <a-form :model="form" @submit="handleSubmit" auto-label-width>
-        <a-form-item field="enabled" label="启用" class="enable-line" help="启用后该策略才会生效">
-          <a-switch type="round" v-model="form.enabled">
-            <template #checked> ON </template>
-            <template #unchecked> OFF </template>
-          </a-switch>
-        </a-form-item>
-        <a-form-item field="name" label="名称" help="配置名称" required>
-          <a-input v-model="form.name"></a-input>
-        </a-form-item>
-        <a-form-item field="describe" label="描述" help="配置用途描述" required>
-          <a-input v-model="form.describe"></a-input>
-        </a-form-item>
+    <div class="content">
+      <a-card>
+        <a-form :model="form" @submit="handleSubmit" auto-label-width>
+          <a-form-item field="enabled" label="启用" class="enable-line" help="启用后该策略才会生效">
+            <a-switch type="round" v-model="form.enabled">
+              <template #checked> ON </template>
+              <template #unchecked> OFF </template>
+            </a-switch>
+          </a-form-item>
+          <a-form-item field="name" label="名称" help="配置名称" required>
+            <a-input v-model="form.name"></a-input>
+          </a-form-item>
+          <a-form-item field="describe" label="描述" help="配置用途描述" required>
+            <a-input v-model="form.describe"></a-input>
+          </a-form-item>
 
-        <a-divider orientation="center" type="dotted">匹配规则</a-divider>
-        <a-form-item
-          field="condition.sub_events_match_type"
-          label="匹配方式"
-          help="通配符: test/*"
-          required
-        >
-          <a-radio-group v-model="form.condition.sub_events_match_type" type="button">
-            <a-radio value="EXACT">精确匹配</a-radio>
-            <a-radio value="GLOB">通配符</a-radio>
-            <a-radio value="REGEXP">正则表达式</a-radio>
-          </a-radio-group>
-        </a-form-item>
-        <a-form-item field="condition.events" label="事件" required>
-          <template #help>
-            <span
-              >选择或者输入事件的名称,具体请参考:
-              <a
-                target="_blank"
-                href="https://docs.gitlab.com/ee/user/project/integrations/webhook_events.html"
-                >Gitlab Webhook events</a
-              >
-            </span>
-          </template>
-          <a-select v-model="form.condition.events" multiple allow-create>
-            <a-option>Push Hook</a-option>
-            <a-option>Tag Push Hook</a-option>
-            <a-option>Issue Hook</a-option>
-            <a-option>Note Hook</a-option>
-            <a-option>Merge Request Hook</a-option>
-          </a-select>
-        </a-form-item>
-        <a-form-item field="condition.sub_events" label="分支" help="输入后 敲回车录入" required>
-          <a-select v-model="form.condition.sub_events" multiple allow-create> </a-select>
-        </a-form-item>
-        <a-divider orientation="center" type="dotted">触发流水线</a-divider>
-        <a-form-item field="pipeline_id" label="流水线" help="规则匹配后执行的流水线" required>
-          <a-select @change="selectedPipelineChange" v-model="form.pipeline_id">
-            <a-option v-for="p in queryPipelineResp.items" :key="p.id" :value="p.id">{{
-              p.name
-            }}</a-option>
-          </a-select>
-        </a-form-item>
-        <a-form-item label="" help="">
-          <PipelineDetail
-            style="width: 100%"
-            backgroundColor="#f7f8fa"
-            v-if="selectedPipeline"
-            :pipeline="selectedPipeline"
-            @updateParam="updateParam"
-            @updateAudit="updateAudit"
-            @updateImRobotNotify="updateImRobotNotify"
-            @updateWebHookNotify="updateWebHookNotify"
-            @updateMentitionUserNotify="updateMentitionUserNotify"
-            :allowAppend="true"
-          ></PipelineDetail>
-        </a-form-item>
-        <div class="form-submit">
-          <a-space>
-            <a-button @click="router.go(-1)">取消</a-button>
-            <a-button type="primary" html-type="submit" :loading="submitLoading">保存</a-button>
-          </a-space>
-        </div>
-      </a-form>
-    </a-card>
+          <a-divider orientation="center" type="dotted">匹配规则</a-divider>
+          <a-form-item
+            field="condition.sub_events_match_type"
+            label="匹配方式"
+            help="通配符: test/*"
+            required
+          >
+            <a-radio-group v-model="form.condition.sub_events_match_type" type="button">
+              <a-radio value="EXACT">精确匹配</a-radio>
+              <a-radio value="GLOB">通配符</a-radio>
+              <a-radio value="REGEXP">正则表达式</a-radio>
+            </a-radio-group>
+          </a-form-item>
+          <a-form-item field="condition.events" label="事件" required>
+            <template #help>
+              <span
+                >选择或者输入事件的名称,具体请参考:
+                <a
+                  target="_blank"
+                  href="https://docs.gitlab.com/ee/user/project/integrations/webhook_events.html"
+                  >Gitlab Webhook events</a
+                >
+              </span>
+            </template>
+            <a-select v-model="form.condition.events" multiple allow-create>
+              <a-option>Push Hook</a-option>
+              <a-option>Tag Push Hook</a-option>
+              <a-option>Issue Hook</a-option>
+              <a-option>Note Hook</a-option>
+              <a-option>Merge Request Hook</a-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item field="condition.sub_events" label="分支" help="输入后 敲回车录入" required>
+            <a-select v-model="form.condition.sub_events" multiple allow-create> </a-select>
+          </a-form-item>
+          <a-divider orientation="center" type="dotted">触发流水线</a-divider>
+          <a-form-item field="pipeline_id" label="流水线" help="规则匹配后执行的流水线" required>
+            <a-select @change="selectedPipelineChange" v-model="form.pipeline_id">
+              <a-option v-for="p in queryPipelineResp.items" :key="p.id" :value="p.id">{{
+                p.name
+              }}</a-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item label="" help="">
+            <PipelineDetail
+              style="width: 100%"
+              backgroundColor="#f7f8fa"
+              v-if="selectedPipeline"
+              :pipeline="selectedPipeline"
+              @updateParam="updateParam"
+              @updateAudit="updateAudit"
+              @updateImRobotNotify="updateImRobotNotify"
+              @updateWebHookNotify="updateWebHookNotify"
+              @updateMentitionUserNotify="updateMentitionUserNotify"
+              :allowAppend="true"
+            ></PipelineDetail>
+          </a-form-item>
+          <div class="form-submit">
+            <a-space>
+              <a-button @click="router.go(-1)">取消</a-button>
+              <a-button type="primary" html-type="submit" :loading="submitLoading">保存</a-button>
+            </a-space>
+          </div>
+        </a-form>
+      </a-card>
+    </div>
   </div>
 </template>
 
