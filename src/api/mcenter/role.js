22b02c0d5727789506d9cf16987a74db81a9986a
yumaojun03 <719118794@qq.com>
2024-05-26 11:44:09 +0800

    补充权限选择

--- src/api/mcenter/role.js
+++ src/api/mcenter/role.js
@@ -9,3 +9,13 @@ export var LIST_ROLE = (params) => {
 export var GET_ROLE = (id) => {
   return instance.get(`/mcenter/api/v1/role/${id}`)
 }
+
+// 查询系统服务
+export var LIST_SYSTEM_SERVICE = (params) => {
+  return instance.get('/mcenter/api/v1/system/service', { params })
+}
+
+// 查询系统资源
+export var LIST_SYSTEM_RESOURCE = (params) => {
+  return instance.get('/mcenter/api/v1/system/resource', { params })
+}
--- src/views/admin/role/CreatePage.vue
+++ src/views/admin/role/CreatePage.vue
@@ -11,10 +11,35 @@
       </a-steps>
       <div class="content">
         <div class="permission" v-if="current === 1">
-          <div class="svc-tree">
-            <a-tree :default-selected-keys="['0-0']" :data="treeData" :show-line="showLine" />
+          <div>
+            <a-tree
+              class="svc-tree"
+              :default-selected-keys="['0-0']"
+              :data="treeData"
+              size="mini"
+              :default-expanded-keys="['root']"
+              :show-line="true"
+              :blockNode="true"
+              :selected-keys="selectedKeys"
+              @select="selectTreeNode"
+            >
+              <template #title="node">
+                {{ nodeTitle(node) }}
+              </template>
+            </a-tree>
           </div>
-          <dir class="svc-perm">perm</dir>
+          <div class="svc-perm">
+            <a-table :data="selectTService().items">
+              <template #columns>
+                <a-table-column title="资源名称" :width="160" data-index="name"></a-table-column>
+                <a-table-column title="资源操作">
+                  <template #cell="{ record }">
+                    <a-checkbox-group v-model="record.selected" :options="record.actions" />
+                  </template>
+                </a-table-column>
+              </template>
+            </a-table>
+          </div>
         </div>
         <div v-if="current === 2">基本信息</div>
         <div v-if="current === 3">权限确认</div>
@@ -32,10 +57,27 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, reactive } from 'vue'
+import { LIST_SYSTEM_SERVICE, LIST_SYSTEM_RESOURCE } from '@/api/mcenter/role'
 
 // 判断更新模式
 let pageHeader = '创建角色'
+
+// 服务树
+const treeData = ref([
+  {
+    title: '服务树',
+    key: 'root',
+    children: [
+      {
+        title: '用户中心',
+        key: 'mcenter',
+        loaded: false,
+        resource: { items: [] }
+      }
+    ]
+  }
+])
 
 // 切换
 const current = ref(1)
@@ -43,17 +85,92 @@ const onPrev = () => {
   current.value = Math.max(1, current.value - 1)
 }
 const onNext = () => {
+  // 判断是否选择有权限
+  console.log(current.value)
+
   current.value = Math.min(3, current.value + 1)
 }
 
-// 服务树
-const treeData = ref([
-  {
-    title: '服务树',
-    key: 'root',
-    children: []
+// 服务切换
+const selectedKeys = ref([])
+const nodeResource = reactive({})
+const selectTreeNode = async (e) => {
+  // 创建临时变量存储选中的节点键
+  const selectedNodeKey = e[0]
+
+  if (selectedKeys.value.length > 0 && selectedKeys.value[0] === selectedNodeKey) {
+    return
   }
-])
+
+  // 忽略Root处理
+  if (selectedNodeKey === 'root') {
+    return
+  }
+
+  selectedKeys.value = [selectedNodeKey]
+
+  // 加载资源
+  if (!nodeResource[selectedNodeKey]) {
+    nodeResource[selectedNodeKey] = { loaded: true }
+    const resp = await LIST_SYSTEM_RESOURCE({
+      service_ids: selectedNodeKey
+    })
+    nodeResource[selectedNodeKey].items = resp.items
+    nodeResource[selectedNodeKey].total = resp.total
+  }
+}
+
+const selectTService = () => {
+  const v = nodeResource[selectedKeys.value[0]]
+  if (v) {
+    return v
+  }
+
+  return { resource: { items: [] } }
+}
+const nodeTitle = (node) => {
+  var title = node.title
+
+  if (node.key === 'root') {
+    return title
+  }
+
+  const resources = nodeResource[node.key]
+  if (!resources || !resources.items) {
+    return title
+  }
+
+  var selected_count = 0
+  var total_count = 0
+
+  resources.items.forEach((item) => {
+    total_count += item.actions.length
+    if (item.selected) {
+      selected_count += item.selected.length
+    }
+  })
+
+  if (selected_count === 0) {
+    return title
+  }
+
+  return `${title} (${selected_count}/${total_count})`
+}
+
+onMounted(async () => {
+  const resp = await LIST_SYSTEM_SERVICE()
+  resp.items.forEach((item) => {
+    treeData.value[0].children.push({
+      title: item.description,
+      key: item.id,
+      loaded: false,
+      resource: { items: [] }
+    })
+  })
+
+  const first = treeData.value[0].children[0]
+  selectTreeNode([first.key], { node: first })
+})
 </script>
 
 <style scoped>
@@ -78,5 +195,10 @@ const treeData = ref([
   border: 1px solid var(--color-border);
   border-radius: 2px 2px 0 0;
   background-color: var(--color-fill-1);
+}
+
+.svc-perm {
+  padding: 0px 12px;
+  width: 100%;
 }
 </style>
